6421ae74dbcf6ad74368934db1995a4071610c32
bruno <brunocolaresdev@gmail.com>
2025-02-15 13:04:49 -0300

    Solução ruim: tratamento sem exceção e condições na classe principal

--- src/application/Program.java
+++ src/application/Program.java
@@ -22,17 +22,31 @@ public class Program {
 		System.out.print("Check-out date (dd/MM/yyyy): ");
 		Date checkOut = simpleDateFormat.parse(sc.nextLine());
 		
+		if(!checkOut.after(checkIn)) {
+			System.out.println("Erro in reservation: Check-out date must be after check-in date");
+		} else {
+			Reservation reservation = new Reservation(123, checkIn, checkOut);
+			System.out.println("Reservation: "+reservation);
+			
+			System.out.println("Enter data to update the reservation: ");
+			System.out.print("Check-in date (dd/MM/yyyy): ");
+			checkIn = simpleDateFormat.parse(sc.nextLine());
+			System.out.print("Check-out date (dd/MM/yyyy): ");
+			checkOut = simpleDateFormat.parse(sc.nextLine());
+			
+			Date now = new Date();
+			if(checkIn.before(now) || checkOut.before(now)) {
+				System.out.println("Error in reservation: Reservation dates for update must be future dates");
+			} else if(!checkOut.after(checkIn)) {
+				System.out.println("Erro in reservation: Check-out date must be after check-in date");
+			} else {
+				reservation.updateDates(checkIn, checkOut);
+				System.out.println("Reservation: "+reservation);
+			}
+		}
 		
-		Reservation reservation = new Reservation(123, checkIn, checkOut);
-		System.out.println(reservation);
 		
-		System.out.println("Enter data to update the reservation: ");
-		System.out.print("Check-in date (dd/MM/yyyy): ");
-		checkIn = simpleDateFormat.parse(sc.nextLine());
-		System.out.print("Check-out date (dd/MM/yyyy): ");
-		checkOut = simpleDateFormat.parse(sc.nextLine());
-		reservation.updateDates(checkIn, checkOut);
-		System.out.println(reservation);
+		
 	}
 
 }
