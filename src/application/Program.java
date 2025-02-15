693c8f0ce61d69772f431950535508b3a972852e
bruno <brunocolaresdev@gmail.com>
2025-02-15 11:51:29 -0300

    first commit

---
+++ src/application/Program.java
@@ -0,0 +1,19 @@
+package application;
+
+import java.time.Duration;
+import java.time.temporal.ChronoUnit;
+import java.time.temporal.TemporalUnit;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.Scanner;
+
+public class Program {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		
+	
+	}
+
+}
